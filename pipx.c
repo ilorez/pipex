29b814461deeddc7bb175356064a1d6fa6c1ced5
ilorez <zobirnajdaoui@gmail.com>
2025-01-07 16:28:54 +0100

    nothing just some tests

--- pipx.c
+++ pipx.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 12:32:17 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/07 08:14:45 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/07 15:54:41 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ int int main(int argc, char *argv[], char *envp[])
 {
   int pid;
   int fd[2];
-  int fd1;
-  int fd2;
   char *line;
-  char *f_v;
   
   if (pipe(fd) == -1)
   {
@@ -39,9 +36,39 @@ int int main(int argc, char *argv[], char *envp[])
   if (pid == 0)
   {
     close(fd[0]);
-    char **cmd_args = ft_split(argv[2], ' ');
-    execve(cmd_args[0], cmd_args, envp);
-    fd1 = open(argv[1], O_RONLY);
+    char *command = ft_strjoin(argv[2], " ");
+    command = ft_strjoin(command, argv[1]);
+    char **cmd_args = ft_split(command, ' ');
+    dup2(fd[1], 1);
+    if(execve(argv[2], cmd_args, envp)==-1)
+    {
+      perror("execve");
+      exit(1);
+    }
+  }
+  else {
+    close(fd[1]);
+    line = get_next_line(fd[0]);
+    while (line)
+    {
+      if (fork() == 0)
+      {
+        int fd1 = open(argv[4], O_WRONLY | O_CREAT, 0777);
+        if (fd1 == -1)
+        {
+          perror("open");
+          exit(1);
+        }
+        dup2(fd1, 1);
+        if(execve(argv[3], cmd_args, envp)==-1)
+        {
+          perror("execve");
+          exit(1);
+        }
+      }
+      free(line);
+      line = get_next_line(fd[0]);
+    }
     free(line);
   }
   return EXIT_SUCCESS;
