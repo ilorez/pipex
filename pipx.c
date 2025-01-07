b10dbe83d6f1d4f4043bd1f2a96aec1e78bbe770
ilorez <zobirnajdaoui@gmail.com>
2025-01-07 18:51:14 +0100

    okay it's worked

--- pipx.c
+++ pipx.c
@@ -6,22 +6,81 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 12:32:17 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/07 15:54:41 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:50:03 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipx.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include "pipx.h"
+#include "libft.h"
 
+t_bool  ft_change_fd(int fd, int to)
+{
+  if (dup2(fd, to) == -1)
+  {
+    perror("dup2");
+    exit(1);
+  }
+  close(fd);
+  return true;
+}
+char **ft_get_paths(char *envp[])
+{
+  char **paths;
+  char *tmp;
 
-int int main(int argc, char *argv[], char *envp[]) 
+  paths = NULL;
+  while (envp)
+  {
+    if (ft_strncmp(*envp, "PATH=", 5) == 0)
+    {
+      paths = ft_split(*envp, ':');
+      break;
+    }
+    envp++;
+  }
+  if (!paths)
+  {
+    // set errno
+    ft_printf("error not fonded");
+    exit(1);
+  }
+  tmp = paths[0];
+  paths[0] = ft_strdup(tmp+5);
+  free(tmp);
+  return paths;
+}
+
+char *ft_get_right_path(char *cmd, char **paths)
+{
+  char *path;
+
+  path = NULL;
+  while (*paths)
+  {
+    path = ft_strjoin(ft_strjoin(paths[0], "/"), cmd);
+    if (access(path, X_OK) == 0)
+      return path;
+    free(path);
+    paths++;
+  }
+  return (ft_strdup(cmd));
+}
+
+int main(int argc, char *argv[], char *envp[]) 
 {
   int pid;
   int fd[2];
-  char *line;
-  
+  int infile;
+  char **cmds;
+  int outfile;
+  char **paths;
+  char *path;
+
+  paths = ft_get_paths(envp);
   if (pipe(fd) == -1)
   {
     perror("pipe");
@@ -36,40 +95,47 @@ int int main(int argc, char *argv[], char *envp[])
   if (pid == 0)
   {
     close(fd[0]);
-    char *command = ft_strjoin(argv[2], " ");
-    command = ft_strjoin(command, argv[1]);
-    char **cmd_args = ft_split(command, ' ');
-    dup2(fd[1], 1);
-    if(execve(argv[2], cmd_args, envp)==-1)
+    infile = open(argv[1], O_RDONLY);
+    if (infile == -1)
     {
-      perror("execve");
+          perror("open infile");
+          exit(1);
+    }
+    ft_change_fd(infile, STDIN_FILENO);
+    ft_change_fd(fd[1], STDOUT_FILENO);
+    cmds = ft_split(argv[2], ' ');
+    path = ft_get_right_path(cmds[0], paths);
+    ft_printf("path: %s\n", path);
+    if(execve(path, cmds, envp) == -1)
+    {
+      perror("execve cmd1");
+      free(path);
+      ft_free_str_lst(cmds);
       exit(1);
     }
   }
   else {
-    close(fd[1]);
-    line = get_next_line(fd[0]);
-    while (line)
+    if (fork() == 0)
     {
-      if (fork() == 0)
+      close(fd[1]);
+      outfile = open(argv[4], O_WRONLY | O_CREAT, 0777);
+      if (outfile == -1)
       {
-        int fd1 = open(argv[4], O_WRONLY | O_CREAT, 0777);
-        if (fd1 == -1)
-        {
-          perror("open");
+          perror("open outfile");
           exit(1);
-        }
-        dup2(fd1, 1);
-        if(execve(argv[3], cmd_args, envp)==-1)
-        {
-          perror("execve");
-          exit(1);
-        }
       }
-      free(line);
-      line = get_next_line(fd[0]);
+      ft_change_fd(fd[0], STDIN_FILENO);
+      ft_change_fd(outfile, STDOUT_FILENO);
+      cmds = ft_split(argv[3], ' ');
+      path = ft_get_right_path(cmds[0], paths);
+      if(execve(path, cmds, envp) == -1)
+      {
+        perror("execve cmd2");
+        ft_free_str_lst(cmds);
+        exit(1);
+      }
     }
-    free(line);
+    wait(NULL);
   }
   return EXIT_SUCCESS;
 }
