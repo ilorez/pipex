e9471c83bf5cf97715561793a1571656e840b5ee
ilorez <zobirnajdaoui@gmail.com>
2025-01-13 18:11:53 +0100

    working on heredoc

--- srcs/pipx.c
+++ srcs/pipx.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 15:53:31 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/13 14:08:40 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/13 17:49:05 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	**paths;
+	t_pipx	*data;
 
 	if (argc < 5)
 	{
-		ft_printf("Usage: \n\t%s infile cmd1 cmd2 ... cmdn outfile\n", argv[0]);
+    ft_printf("Usage: \n\t%s infile cmd1 cmd2 ... cmdn outfile\n", argv[0]);
 		return (EXIT_FAILURE);
-	}
-
+  }
 	paths = ft_get_paths(envp);
-  //if (strncmp(argv[1], "heredoc") == 0)
-  //{
-  //  continue;
+  data = ft_calloc(sizeof(t_pipx), 1);
+  if (strncmp(argv[1], "heredoc", ft_strlen(argv[1]) + 1) == 0)
+	  data->i = 2;
+  data->argc = argc;
   //  // get all inputs and put a file discriptior you can use a pip
   //  // after that edit in run commands to accept already open infile
   //  // edit ft_run_commands to open outfile with append flag in case of heredoc
   //  // just done i mean is it like that you should correct some one pipx
-  //}
-	if (!ft_run_commands(argc, argv, envp, paths))
+	if (!ft_run_commands(data, argv, envp, paths))
 		return (free(paths), EXIT_FAILURE);
 	free(paths);
+	}
 	return (0);
 }
--- srcs/run_commands.c
+++ srcs/run_commands.c
@@ -6,15 +6,39 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 13:33:00 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/13 14:47:14 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:11:36 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipx.h"
 
-void	ft_child(t_pipx *data, int i, int argc, char *envp[])
+t_bool	ft_run_commands(t_pipx *data, char *argv[], char *envp[], char *paths[])
 {
-	if (i < argc - 2)
+	if (ft_open_rw_files(&data, argv, argc) == false)
+		return (false);
+	while (++(data->i) < data->argc - 1)
+	{
+		data->cmds = ft_split(argv[data->i], ' ');
+		data->path = ft_get_right_path(data->cmds[0], paths);
+		if (pipe(data->fd) == -1)
+			return (ft_on_error(data, data->cmds, data->path, "pipe"));
+		data->pid = fork();
+		if (data->pid == -1)
+			return (ft_on_error(data, data->cmds, data->path, "fork"));
+		else if (data->pid == 0)
+			ft_child(data, envp);
+		close((data->fd)[1]);
+		data->infile = (data->fd)[0];
+		ft_on_error(data, data->cmds, data->path, NULL);
+	}
+	close(data->infile);
+	wait(NULL);
+	return (true);
+}
+
+void	ft_child(t_pipx *data, char *envp[])
+{
+	if (data->i < data->argc - 2)
 		ft_change_fd((data->fd)[1], 1);
 	else
   {
@@ -32,39 +56,15 @@ void	ft_child(t_pipx *data, int i, int argc, char *envp[])
 
 t_bool	ft_open_rw_files(t_pipx **data, char *argv[], int argc)
 {
-	*data = ft_calloc(sizeof(t_pipx), 1);
-	(*data)->i = 1;
 	(*data)->infile = open(argv[1], O_RDONLY);
 	if ((*data)->infile == -1)
 		return (ft_on_error((*data), NULL, NULL, "open infile"));
-	(*data)->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT, 0777);
+  if ((*data)->i == 2)
+	    (*data)->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+  else
+	    (*data)->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if ((*data)->outfile == -1)
 		return (ft_on_error(*data, NULL, NULL, "open outfile"));
 	return (true);
 }
 
-t_bool	ft_run_commands(int argc, char *argv[], char *envp[], char *paths[])
-{
-	t_pipx	*data;
-
-	if (ft_open_rw_files(&data, argv, argc) == false)
-		return (false);
-	while (++(data->i) < argc - 1)
-	{
-		data->cmds = ft_split(argv[data->i], ' ');
-		data->path = ft_get_right_path(data->cmds[0], paths);
-		if (pipe(data->fd) == -1)
-			return (ft_on_error(data, data->cmds, data->path, "pipe"));
-		data->pid = fork();
-		if (data->pid == -1)
-			return (ft_on_error(data, data->cmds, data->path, "fork"));
-		else if (data->pid == 0)
-			ft_child(data, data->i, argc, envp);
-		close((data->fd)[1]);
-		data->infile = (data->fd)[0];
-		ft_on_error(data, data->cmds, data->path, NULL);
-	}
-	close(data->infile);
-	wait(NULL);
-	return (true);
-}
